52cea42de34e1297aa12453713e44e42c4e3d5d2
Made Indra <made.indra@protonmail.com>
2022-12-30 06:46:02 +0700

    create basic server

---
+++ src/server2.js
@@ -0,0 +1,28 @@
+// import dependencies
+import { createServer } from 'node:http';
+import { parse } from 'node:url';
+import { randomUUID } from 'node:crypto';
+
+// config
+const PORT = 3002;
+
+// handler
+async function handler(req, res) {
+  if (req.method === 'GET' && req.url.startsWith('/products')) {
+    const { query: { name } }  = parse(req.url, true);
+    
+    const item = {
+      id: randomUUID(),
+      product: name,
+    }
+
+    return res.end(JSON.stringify(item));
+  }
+
+  return res.end('Hello World!');
+}
+
+// create server
+createServer(handler).listen(PORT).on('listening', () => {
+  console.log(`Server listening on port ${PORT}`);
+});
